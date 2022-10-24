2d98589850cfeb366cb90706fe59d9cd4f31a53d
potasiuombun <tudor_ph@yahoo.com>
2022-10-24 10:13:06 +0200

    Replace image folder

--- ImgResizer.py
+++ ImgResizer.py
@@ -3,12 +3,12 @@ import os
 import glob
 
 
-# Resize all the images to (50,60)
+# Resize all the images to (60,80)
 input_folder = 'images'
 os.mkdir("Resized")
 i = 0
 for img in glob.glob(input_folder + "/*.jpg"):
     image = cv2.imread(img)
-    imgRe = cv2.resize(image,(50,60),interpolation = cv2.INTER_AREA)
+    imgRe = cv2.resize(image,(60,80),interpolation = cv2.INTER_AREA)
     cv2.imwrite("Resized/image%04i.jpg" %i,imgRe)
     i +=1
