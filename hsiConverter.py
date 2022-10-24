193cf939753863be791c9aee90c2ae63fa55eafd
potasiuombun <tudor_ph@yahoo.com>
2022-10-24 10:57:28 +0200

    Create hsiConverter.py

---
+++ hsiConverter.py
@@ -0,0 +1,8 @@
+folder = 'Resized'
+os.mkdir("HSI_images")
+i = 0
+for img in glob.glob(folder + "/*.jpg"):
+    image = cv2.imread(img)
+    imgHSI = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+    cv2.imwrite("HSI_images/hsi%04i.jpg" %i,imgHSI)
+    i +=1
