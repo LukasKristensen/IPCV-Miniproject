b7b517af6655e00c77cb110671168e5171bcef12
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-10-24 11:30:55 +0200

    Canny detection

--- SignalClassifier.py
+++ SignalClassifier.py
@@ -1,8 +1,9 @@
+import os
+
 import cv2
 import numpy as np
 
 # TODO:
-#   - Rectangle extraction (the signal itself)
 #   - Circle extraction finding the lights
 #       - Validate with circularity?
 #   - Classification of which lights are active
@@ -33,7 +34,41 @@ def circle_template_match(input_image):
     cv2.waitKey(0)
 
 
-if __name__ == '__main__':
-    test_converted = cv2.imread('images/test_image_active_lights.png',0)
-    circle_template_match(test_converted)
+def ellipse_find(input_image):
+    # Documentation: https://docs.opencv.org/3.4/de/d62/tutorial_bounding_rotated_ellipses.html
 
+    cv2.imshow("Original input", input_image)
+
+    input_hsv = cv2.cvtColor(input_image, cv2.COLOR_BGR2HSV)
+    input_hsv = input_hsv[:,:,2]
+    cv2.imshow("HSV input",input_hsv)
+    # cv2.waitKey(0)
+
+    canny = cv2.Canny(input_hsv, 100, 250)
+    cv2.imshow("Canny output",canny)
+
+    # Documentation for contours and circularity: https://www.authentise.com/post/detecting-circular-shapes-using-contours
+    contours, hierachy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # print("Contours:",contours[0].shape,contours[1].shape,contours[2].shape)
+
+
+    cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    # test_converted = cv2.imread('images/test_image_active_lights.png',0)
+    # circle_template_match(test_converted)
+
+    # input_image_load = cv2.imread('images/GOOD_TRACK_PU_1_AND_4---4_45_1.jpg')
+    # ellipse_find(input_image_load)
+
+    # input_image_load = cv2.imread('images/GOOD_TRACK_DWARF_1_AND_4---6_12803_0.jpg')
+    # ellipse_find(input_image_load)
+
+    for dataset_image in os.listdir('Resized'):
+        image_file = os.path.join('Resized', dataset_image)
+        print("Showing:",image_file)
+
+        if os.path.isfile(image_file):
+            input_image_load = cv2.imread(image_file)
+            ellipse_find(input_image_load)
