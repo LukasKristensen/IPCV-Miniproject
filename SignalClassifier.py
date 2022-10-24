741470c66ac4ec8069cbd0de80b839ecd69e3a4f
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-10-24 13:25:05 +0200

    edge detection

--- SignalClassifier.py
+++ SignalClassifier.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import cv2
 import numpy as np
@@ -10,34 +11,39 @@ import numpy as np
 #   - Evaluate performance with confusion matrix
 
 
-def circle_template_match(input_image):
-    circle_template = cv2.imread('templates/circle.png',0)
-    circle_template = cv2.resize(circle_template, (20,20))
+def circle_template_match(input_image, original_image):
+    circle_template = cv2.imread('templates/edge_light.png',0)
+    # circle_template = cv2.resize(circle_template, (20,20))
+
+    print("Shape input:",input_image.shape,"Template shape:",circle_template.shape)
 
     # Documentation for template matching: https://docs.opencv.org/4.x/d4/dc6/tutorial_py_template_matching.html
     template_result = cv2.matchTemplate(input_image,circle_template,cv2.TM_CCOEFF_NORMED)
     print(template_result)
 
     cv2.imshow("result", template_result)
-    cv2.waitKey(0)
+    # cv2.waitKey(0)
 
     locations = np.where(template_result >= 0.4)
-    print("Locations:",locations)
+    # print("Locations:",locations)
+    cv2.imshow("Template result", template_result)
 
-    highlighted_templates = cv2.imread('images/test_image_active_lights.png')
+    highlighted_templates = original_image
 
     for point in zip(*locations[::-1]):
         print("Point:",point)
-        cv2.rectangle(highlighted_templates, point, (point[0]+20,point[1]+20),(0,255,255),2)
+        cv2.rectangle(highlighted_templates, point, (point[0]+20,point[1]+20),(random.randint(0,255),random.randint(0,255),random.randint(0,255)),1)
 
     cv2.imshow("result threshold", highlighted_templates)
-    cv2.waitKey(0)
+    # cv2.waitKey(0)
 
 
 def ellipse_find(input_image):
     # Documentation: https://docs.opencv.org/3.4/de/d62/tutorial_bounding_rotated_ellipses.html
+    input_image = cv2.cvtColor(input_image, cv2.COLOR_GRAY2BGR)
 
     cv2.imshow("Original input", input_image)
+    # cv2.waitKey()
 
     input_hsv = cv2.cvtColor(input_image, cv2.COLOR_BGR2HSV)
     input_hsv = input_hsv[:,:,2]
@@ -50,6 +56,8 @@ def ellipse_find(input_image):
     # Documentation for contours and circularity: https://www.authentise.com/post/detecting-circular-shapes-using-contours
     contours, hierachy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # print("Contours:",contours[0].shape,contours[1].shape,contours[2].shape)
+
+    circle_template_match(canny, input_image)
 
 
     cv2.waitKey(0)
@@ -70,5 +78,5 @@ if __name__ == '__main__':
         print("Showing:",image_file)
 
         if os.path.isfile(image_file):
-            input_image_load = cv2.imread(image_file)
+            input_image_load = cv2.imread(image_file, 0)
             ellipse_find(input_image_load)
