c87a0f32f4b1c35e0a61d19fa989ee3761f1efbb
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-10-25 12:14:56 +0200

    optimize

--- SignalClassifier.py
+++ SignalClassifier.py
@@ -40,6 +40,9 @@ def circle_template_match(canny_image, original_image, signal_template):
         return False
 
 
+matches = {}
+
+
 def ellipse_find(input_image,path_input_image):
     # Documentation: https://docs.opencv.org/3.4/de/d62/tutorial_bounding_rotated_ellipses.html
     input_image = cv2.cvtColor(input_image, cv2.COLOR_GRAY2BGR)
@@ -52,27 +55,54 @@ def ellipse_find(input_image,path_input_image):
     canny = cv2.Canny(input_hsv, 100, 250)
     cv2.imshow("Canny output",canny)
 
+    signal_class = None
+    template_class = None
+
     for template_image in os.listdir('templates'):
         template_location = os.path.join('templates', template_image)
         if os.path.isfile(template_location):
             template_file = cv2.imread(template_location,0)
             result_template = circle_template_match(canny, input_image, template_file)
+
+            signal_class = None
+            template_class = None
+
+            #
+            signal_class_read = path_input_image.split("_AND_")
+            if len(signal_class_read) >= 2:
+                signal_class = int(signal_class_read[0][-1]), int(signal_class_read[1][0])
+
             if result_template:
-                print("Found result",template_location)
-                print("True input:",path_input_image)
-                cv2.waitKey()
+                if "can_pass" in template_location:
+                    template_class = 1, 4
+                elif "pass_prohibited" in template_location:
+                    template_class = 1, 2
+
+                if not (signal_class, template_class) in matches:
+                    matches[(signal_class, template_class)] = 1
+                else:
+                    matches[(signal_class, template_class)] += 1
+                if signal_class != template_class:
+                    print("MISS:",signal_class,template_class)
+                    print("TEMPLATE:",template_location)
+                # cv2.waitKey()  # If manual review
                 break
             else:
-                print("No result:",template_location)
-    cv2.waitKey()
+                pass
+    if not (signal_class, template_class) in matches:
+        matches[(signal_class, template_class)] = 1
+    else:
+        matches[(signal_class, template_class)] += 1
 
 
 if __name__ == '__main__':
-    for dataset_image in os.listdir('Resized'):
-        image_file = os.path.join('Resized', dataset_image)
+    for dataset_image in os.listdir('DATA_SET/validation_imgs'):
+        image_file = os.path.join('DATA_SET/validation_imgs', dataset_image)
         print("Showing:",image_file)
 
         if os.path.isfile(image_file):
             input_image_load = cv2.imread(image_file, 0)
             found_signal = ellipse_find(input_image_load,image_file)
+
+    print("RESULT:",matches)
 
