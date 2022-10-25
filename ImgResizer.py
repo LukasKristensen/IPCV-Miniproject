e3dcaae4047ea1ac3aacb8af0ba4e7ccd51b2284
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-10-25 10:49:39 +0200

    Added new version of resizer

--- ImgResizer.py
+++ ImgResizer.py
@@ -14,3 +14,24 @@ def resizeImg(input_folder):
         imgRe = cv2.resize(image,(60,80),interpolation = cv2.INTER_AREA)
         cv2.imwrite("Resized/image%04i.jpg" %i,imgRe)
         i +=1
+
+
+def resizeImg(input_folder):
+    # os.mkdir("Resized2")
+    i = 0
+
+    for image_file in os.listdir(input_folder):
+        image_path = os.path.join(input_folder, image_file)
+        # image_read = glob.glob(image_path)
+        image_resize = cv2.resize(cv2.imread(image_path), (60,80), interpolation=cv2.INTER_AREA)
+        cv2.imwrite(f'Resized2/{image_file}', image_resize)
+        i += 1
+        print("Image file:",image_file)
+
+
+if __name__ == '__main__':
+    folder_path = input("Enter the input path for image folder")
+    resizeImg(folder_path)
+
+
+
