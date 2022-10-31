0faa5557141644464de4f9d5270e8b1c476ea4f8
Lukas Kristensen <lukasbkristensen@gmail.com>
2022-10-31 10:35:15 +0100

    clean-up

--- SignalClassifier.py
+++ SignalClassifier.py
@@ -1,43 +1,22 @@
 import os
 import random
-
 import cv2
-import numpy as np
-
-# TODO:
-#   - Circle extraction finding the lights
-#       - Validate with circularity?
-#   - Classification of which lights are active
-#   - Evaluate performance with confusion matrix
 
 
 def circle_template_match(canny_image, original_image, signal_template):
     # Documentation for template matching: https://docs.opencv.org/4.x/d4/dc6/tutorial_py_template_matching.html
     template_result = cv2.matchTemplate(canny_image,signal_template,cv2.TM_CCOEFF_NORMED)
+    minimum_value, maximum_value, minimum_location, maximum_location = cv2.minMaxLoc(template_result)
+
     cv2.imshow("result", template_result)
-
-    print("Height template:",len(signal_template),"width:",len(signal_template[0]))
-
-    # print("Signal template shape:",signal_template.shape)
-
-    locations = np.where(template_result >= 0.4)
     cv2.imshow("Template result", template_result)
 
     highlighted_templates = original_image
-    template_found = False
+    cv2.rectangle(highlighted_templates, minimum_location,
+                  (minimum_location[1] + len(signal_template[0]), minimum_location[0] + len(signal_template)),
+                  (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), 2)
 
-    for point in zip(*locations[::-1]):
-        print("Point:",point)
-        template_found = True
-        cv2.rectangle(highlighted_templates, point, (point[0]+len(signal_template[0]),point[1]+len(signal_template)),(random.randint(0,255),random.randint(0,255),random.randint(0,255)),2)
-        break
-
-    cv2.imshow("result threshold", highlighted_templates)
-
-    if template_found:
-        return True
-    else:
-        return False
+    return maximum_value
 
 
 matches = {}
@@ -55,8 +34,13 @@ def ellipse_find(input_image,path_input_image):
     canny = cv2.Canny(input_hsv, 100, 250)
     cv2.imshow("Canny output",canny)
 
-    signal_class = None
+    signal_class_read = path_input_image.split("_AND_")
+    signal_class = int(signal_class_read[0][-1]), int(signal_class_read[1][0])
+
     template_class = None
+
+    best_template_value = 0
+    best_template_location = None
 
     for template_image in os.listdir('templates'):
         template_location = os.path.join('templates', template_image)
@@ -64,35 +48,24 @@ def ellipse_find(input_image,path_input_image):
             template_file = cv2.imread(template_location,0)
             result_template = circle_template_match(canny, input_image, template_file)
 
-            signal_class = None
-            template_class = None
-
-            #
-            signal_class_read = path_input_image.split("_AND_")
-            if len(signal_class_read) >= 2:
-                signal_class = int(signal_class_read[0][-1]), int(signal_class_read[1][0])
-
-            if result_template:
-                if "can_pass" in template_location:
-                    template_class = 1, 4
-                elif "pass_prohibited" in template_location:
-                    template_class = 1, 2
-
-                if not (signal_class, template_class) in matches:
-                    matches[(signal_class, template_class)] = 1
-                else:
-                    matches[(signal_class, template_class)] += 1
-                if signal_class != template_class:
-                    print("MISS:",signal_class,template_class)
-                    print("TEMPLATE:",template_location)
+            if result_template > best_template_value or best_template_value == 0:
+                best_template_value = result_template
+                best_template_location = template_location
                 # cv2.waitKey()  # If manual review
-                break
-            else:
-                pass
-    if not (signal_class, template_class) in matches:
-        matches[(signal_class, template_class)] = 1
-    else:
-        matches[(signal_class, template_class)] += 1
+
+    if best_template_value is not None:
+        if "can_pass" in best_template_location:
+            template_class = 1, 4
+        elif "pass_prohibited" in best_template_location:
+            template_class = 1, 2
+
+        if not (signal_class, template_class) in matches:
+            matches[(signal_class, template_class)] = 1
+        else:
+            matches[(signal_class, template_class)] += 1
+        if signal_class != template_class:
+            print("MISS:", signal_class, template_class)
+            print("TEMPLATE:", best_template_location)
 
 
 if __name__ == '__main__':
@@ -102,7 +75,7 @@ if __name__ == '__main__':
 
         if os.path.isfile(image_file):
             input_image_load = cv2.imread(image_file, 0)
-            found_signal = ellipse_find(input_image_load,image_file)
+            ellipse_find(input_image_load,image_file)
 
     print("RESULT:",matches)
 
